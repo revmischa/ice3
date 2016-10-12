312c08726b99445c574af9f0153c6778f0e27cf1
Mischa Spiegelmock <revmischa@cpan.org>
2016-10-12 02:36:34 -0700

    reconnect, restarter

--- restart.py
+++ restart.py
@@ -8,16 +8,13 @@ CLUSTER = 'klulz'
 
 
 def restart_service(service):
-    taskdef = service['taskDefinition']
-    sarn = service['serviceArn']
-
     res = client.list_tasks(
         cluster=CLUSTER,
     )
     task_arns = res['taskArns']
     if not task_arns:
         print("Failed to get task_arns :(")
-        return
+        return []
 
     for tarn in task_arns:
         # scale down
@@ -28,23 +25,18 @@ def restart_service(service):
             reason="restarting...",
         )
 
-    if task_arns:
-        waiter = client.get_waiter('tasks_stopped')
-        print("Waiting on", task_arns)
-        waiter.wait(
-            cluster=CLUSTER,
-            tasks=task_arns,
-        )
+    waiter = client.get_waiter('tasks_stopped')
+    print("Waiting on", task_arns)
+    waiter.wait(
+        cluster=CLUSTER,
+        tasks=task_arns,
+    )
+    return task_arns
 
-    # scale up
-    # res = client.run_task(
-    #     cluster=CLUSTER,
-    #     taskDefinition=taskdef,
-    # )
-    # started_tasks = res['tasks']
-    # task_arns = [t['taskArn'] for t in started_tasks]
-    print("Starting...", task_arns)
 
+def wait_on_tasks(task_arns):
+    if not task_arns:
+        return
     waiter = client.get_waiter('tasks_running')
     waiter.wait(
         cluster=CLUSTER,
@@ -64,5 +56,10 @@ if __name__ == '__main__':
         services=services
     )
 
+    task_arns = []
     for svc in services_info['services']:
-        restart_service(svc)
+        tarns = restart_service(svc)
+        task_arns.extend(tarns)
+
+    print("Starting...", task_arns)
+    wait_on_tasks(task_arns)
