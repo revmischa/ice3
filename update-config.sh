124d835f267b5888894f9b179e9c54a4a7452b28
Mischa Spiegelmock <revmischa@cpan.org>
2016-10-29 11:40:34 -0700

    config

--- update-config.sh
+++ update-config.sh
@@ -4,8 +4,11 @@ for k in STREAM_URL STREAM_PASS STREAM_NAME INFO_URL GENRE DESCRIPTION
 do
 	if [[ -z "${!k}" ]]; then
 	    echo "Warn: $k not set"
+	    continue
 	fi
-	echo "$k=${!k}"
-	sed -i "s'%${k}%'${!k}'" ezstream.xml
+	v=${!k}
+	echo "$k=$v"
+	kr="%${k}%"
+	sed -i "s'$kr'$v'" ezstream.xml
 done
 echo "Using stream URL $STREAM_URL"
