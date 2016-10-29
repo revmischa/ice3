1346a1ab235023888c5bfa6e5a34fc9cc6eaefba
Mischa Spiegelmock <revmischa@cpan.org>
2016-10-29 11:17:47 -0700

    more config vars

--- update-config.sh
+++ update-config.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-if [[ -z "${stream_uri}" ]]; then
-    echo "\$stream_uri environment variable is required"
-    exit 1
-fi
-if [[ -z "${stream_pass}" ]]; then
-    echo "\$stream_pass environment variable is required"
-    exit 1
-fi
-
-sed -i "s'%STREAM_URI%'$stream_uri'" ezstream.xml
-sed -i "s'%STREAM_PASS%'$stream_pass'" ezstream.xml
-
-echo "Using stream URI $stream_uri"
+for k in STREAM_URL STREAM_PASS STREAM_NAME INFO_URL GENRE DESCRIPTION
+do
+	if [[ -z "${!k}" ]]; then
+	    echo "Warn: $k not set"
+	fi
+	echo "$k=${!k}"
+	sed -i "s'%${k}%'${!k}'" ezstream.xml
+done
+echo "Using stream URL $STREAM_URL"
